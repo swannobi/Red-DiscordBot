e0acfadb147acdf120dd6f667b151b3cae2497d6
Savestate <joseph.elkhouri+github@gmail.com>
2017-12-09 23:27:54 +0000

    send json request to strawpoll and get the poll id

--- cogs/strawpoll.py
+++ cogs/strawpoll.py
@@ -1,5 +1,9 @@
 # Strawpoll functionality by Savestate for Red-DiscordBot
 
+import urllib3
+import certifi
+import json
+import traceback
 import discord
 from discord.ext import commands
 
@@ -11,17 +15,34 @@ class Strawpoll:
         
     @commands.command(pass_context=True)
     async def strawpoll(self, ctx):
-        """Create and link a new Strawpoll froom user input"""
-        test_embed = discord.Embed(
-            title = "strawpoll embed title", type = "rich", 
-            description = "strawpoll embed description", 
-            url = "http://example.net", 
-            colour = discord.Colour(0x206010))
-        # add_field(*, name, value, inline=True)
-        test_embed.add_field(name="test_inline_true", value="test_value", inline=True)
-        test_embed.add_field(name="test_inline_false", value="test_value2", inline=False)
-        test_embed.set_image(url="https://pbs.twimg.com/media/DQZI3ApV4AAitZc.jpg") #thanks mushbuh
-        await self.bot.send_message(ctx.message.channel, content=None, embed=test_embed)
-        
+        https = urllib3.PoolManager(
+            cert_reqs='CERT_REQUIRED',
+            ca_certs=certifi.where())
+        data = {
+            'title': 'test poll title',
+            'options': [
+                'option 1',
+                'option 2', 
+                'option 3',
+                'option 4???'],
+            'multi': True,
+            'dupcheck': 'normal',
+            'captcha': False }
+        encoded_data = json.dumps(data).encode('utf-8')
+        try:
+            request = https.request(
+                'POST', 'https://strawpoll.me/api/v2/polls',
+                body=encoded_data, headers={
+                    'Content-Type': 'application/json',
+                    'Accept-Charset': 'utf-8'
+                },
+                retries=3, timeout=3.0)
+            json_response = json.loads(request.data.decode('utf-8'))
+        except urllib3.exceptions.RequestError as e:
+            await self.bot.send_message(ctx.message.channel, "oooooops lmao")
+            return
+        await self.bot.send_message(ctx.message.channel,
+            ''.join(['http://www.strawpoll.me/', str(json_response['id'])]))
+
 def setup(bot):
     bot.add_cog(Strawpoll(bot))
