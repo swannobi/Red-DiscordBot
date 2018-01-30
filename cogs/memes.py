631ae572aab91ce9deab34070c1566f114364580
Taylor Swann <swann@ip-172-31-34-102.ec2.internal>
2018-01-30 16:25:54 +0000

    Change trivia messages and add new fun commands

--- cogs/memes.py
+++ cogs/memes.py
@@ -13,14 +13,29 @@ class Memes:
         with open('data/memes/hornet.png', 'rb') as f:
             await self.bot.send_file(ctx.message.channel, f)
 
-#    @commands.command(pass_context=True)
+    @commands.command(pass_context=True)
     async def yay(self, ctx):
         """doens't count"""
         with open('data/memes/doenst.png', 'rb') as f:
             await self.bot.send_file(ctx.message.channel, f)
 
+    @commands.group(name="smith", pass_context=True)
+    async def smith(self, ctx):
+        """Smiff"""
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("https://gfycat.com/WideeyedLazyGoldfish")
+
+    @smith.command(pass_context=True)
+    async def dance(self):
+        await self.bot.say("https://gfycat.com/WideeyedLazyGoldfish")
+
+    @smith.command(pass_context=True)
+    async def crab(self):
+        """Crabwalk"""
+        await self.bot.say("https://gfycat.com/DirtyTartChinesecrocodilelizard")
+
     @commands.command(pass_context=True)
-    async def nugs(self, ctx):
+    async def savestate(self, ctx):
         """Absolute garbage"""
         with open('data/memes/nugs.jpg', 'rb') as f:
             await self.bot.send_file(ctx.message.channel, f)
